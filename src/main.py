b963b89da1645b12ab098d9f2c389e6e136cc1f7
Magnus Küderli <magnusku@gmx.de>
2023-11-14 19:50:43 +0100

    polished cli help interface and added ansi coloring

--- src/main.py
+++ src/main.py
@@ -9,24 +9,30 @@ from color import *
 from camera import *
 import rendertarget, material
 
+defaultSamples = 50
+defaultAR = 16/9
+defaultImageWidth = 400
+defaultBounceCap = 6
 
-parser = argparse.ArgumentParser(description='A simple python raytracer')
-
-parser.add_argument('--scene', "-S", type=str, help='Load a scene from world.json')
-parser.add_argument('--samples', "-s", type=int, help='Number of samples per pixel')
-parser.add_argument('--bounces', "-b", type=int, help='Ray bounce cap. Default is 6')
-parser.add_argument('--res', "-r", type=str, help='Image resolution as WIDTHxHEIGHT (Bsp: "600x400")')
+parser = argparse.ArgumentParser(description='\x1b[1;32;40mA simple python raytracer\x1b[0m')
+color = '\x1b[0;33;40m'
+colorEnd = '\x1b[0m'
+parser.add_argument('--scene', "-S", "-scn", type=str, help=color+'Load a scene from world.json. No defaults'+colorEnd)
+parser.add_argument('--samples', "-s", "-smp", type=int, help=color+f'Number of samples per pixel. Default is {defaultSamples}'+colorEnd)
+parser.add_argument('--bounces', "-b", "-bnc", type=int, help=color+f'Ray bounce cap. Default is {defaultBounceCap}'+colorEnd)
+parser.add_argument('--resolution', "-r", "-res", type=str, help=color+f'Image resolution as WIDTHxHEIGHT (Bsp: "600x400"). Default is {defaultImageWidth}x{max(1, int(defaultImageWidth / defaultAR))}'+colorEnd)
 args = parser.parse_args()
 
 res = None
 if args.res:
     res = [int(args.res.split("x")[0]), int(args.res.split("x")[1])]
 
+
 camera = Camera(
-    samples=args.samples if args.samples else 3, 
-    aspectRatio= max(1, float(res[0]/res[1])) if res else 16/9,
-    imgWidth=res[0] if res else 400, # note: if not given the height is calculated from the width and the aspect ratio
-    maxBounces=args.bounces if args.bounces else 3, 
+    samples=args.samples if args.samples else defaultSamples, 
+    aspectRatio= max(1, float(res[0]/res[1])) if res else defaultAR,
+    imgWidth=res[0] if res else defaultImageWidth, # note: if not given the height is calculated from the width and the aspect ratio
+    maxBounces=args.bounces if args.bounces else defaultBounceCap, 
     ambientOcclusion=col(.4, .4, .8),
     useSky=True
 )
