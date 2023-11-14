8004485326e3e78de54f0f68a13295d1b6fcfbc2
Magnus Küderli <magnusku@gmx.de>
2023-11-14 20:37:51 +0100

    swaped color engine

--- src/main.py
+++ src/main.py
@@ -1,4 +1,7 @@
-import sys, random, argparse, pathlib
+import sys, random, argparse, pathlib 
+from colorama import init as colorama_init
+from colorama import Fore, Back, Style
+colorama_init()
 
 import os
 os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide" # disable annoying pygame welcome message
@@ -15,14 +18,13 @@ defaultImageWidth = 5
 defaultBounceCap = 6
 defaultDest = pathlib.Path(__file__).resolve().parent / pathlib.Path("../output.ppm")
 
-parser = argparse.ArgumentParser(description='\x1b[1;32;40mA simple python raytracer\x1b[0m')
-color = '\x1b[0;33;40m'
-colorEnd = '\x1b[0m'
-parser.add_argument('--scene', "-S", "-scn", type=str, help=color+'Load a scene from world.json. No defaults'+colorEnd)
-parser.add_argument('--samples', "-s", "-smp", type=int, help=color+f'Number of samples per pixel. Default is {defaultSamples}'+colorEnd)
-parser.add_argument('--bounces', "-b", "-bnc", type=int, help=color+f'Ray bounce cap. Default is {defaultBounceCap}'+colorEnd)
-parser.add_argument('--resolution', "-r", "-res", type=str, help=color+f'Image resolution as WIDTHxHEIGHT (Bsp: "600x400"). Default is {defaultImageWidth}x{max(1, int(defaultImageWidth / defaultAR))}'+colorEnd)
-parser.add_argument('--destination', "-d", "-dest", type=str, help=color+f'Path to the rendered image. Default is {pathlib.Path(__file__).resolve().parent / pathlib.Path("../output.ppm")}'+colorEnd)
+parser = argparse.ArgumentParser(description=f'{Back.GREEN}{Fore.BLACK} - A simple python raytracer - {Style.RESET_ALL}')
+
+parser.add_argument('--scene', "-S", "-scn", type=str, help=f"{Fore.GREEN}Load a scene from world.json. {Fore.RED}No defaults{Style.RESET_ALL}")
+parser.add_argument('--samples', "-s", "-smp", type=int, help=f'{Fore.GREEN}Number of samples per pixel. {Fore.RED}Default is {defaultSamples}{Style.RESET_ALL}')
+parser.add_argument('--bounces', "-b", "-bnc", type=int, help=f'{Fore.GREEN}Ray bounce cap. {Fore.RED}Default is {defaultBounceCap}{Style.RESET_ALL}')
+parser.add_argument('--resolution', "-r", "-res", type=str, help=f'{Fore.GREEN}Image resolution as WIDTHxHEIGHT (Bsp: "600x400"). {Fore.RED}Default is {defaultImageWidth}x{max(1, int(defaultImageWidth / defaultAR))}{Style.RESET_ALL}')
+parser.add_argument('--destination', "-d", "-dest", type=str, help=f'{Fore.GREEN}Path to the rendered image. {Fore.RED}Default is {pathlib.Path(__file__).resolve().parent / pathlib.Path("../output.ppm")}{Style.RESET_ALL}')
 args = parser.parse_args()
 
 res = None
